8a320f6df91869b3bb3eb20de9f1d6927d3fb046
Seraphim Dmitrieff <dmitries1@hawkmail.newpaltz.edu>
2018-10-22 14:49:13 -0400

    Update for 10/22 class

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app" class="container">
-    <img alt="Vue logo" src="../assets/logo.png">
+    <img alt="Vue logo" src="./assets/logo.png">
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
