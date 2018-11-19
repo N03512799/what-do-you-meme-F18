f58608ca23194aaf0e61273beed13fd735df977d
Seraphim Dmitrieff <dmitries1@hawkmail.newpaltz.edu>
2018-11-19 09:54:16 -0500

    Sync with Main

---
+++ src/components/PicturePicker.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1>The Picture Picker</h1>
+    </div>
+</template>
+<script>
+export default {
+    data: function () {
+
+    },
+    props: [pictures]
+}
+</script>
+<style lang="scss">
+
+</style>
